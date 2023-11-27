af2b25ba35206752852f0dd325fcaa8560a8df30
blindblood <patrondawn@gmail.com>
2023-11-27 19:34:27 +0300

    added checkbox to datagrid fix focus white border маска poesessid setting window design

--- Poe.UIW/ViewModels/SettingsViewModel.cs
+++ Poe.UIW/ViewModels/SettingsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;

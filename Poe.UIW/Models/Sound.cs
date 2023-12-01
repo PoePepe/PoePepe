30d627e4c6500dca6edda4ac5532fef3e3d95c8d
Konstantin Spitsin <konstantin.spicin@simbirsoft.com>
2023-12-01 20:33:07 +0300

    updated sound settings.

---
+++ Poe.UIW/Models/Sound.cs
@@ -0,0 +1,33 @@
+﻿namespace Poe.UIW.Models;
+
+public class Sound
+{
+    public Sound(string path)
+    {
+        Path = path;
+        _isCustom = false;
+    }
+
+    private Sound(string path, bool isCustom = true)
+    {
+        Path = path;
+        _isCustom = isCustom;
+    }
+
+    public string Name
+    {
+        get
+        {
+            var name = Path?[..Path.LastIndexOf('.')];
+            return _isCustom ? $"Custom {name}" : name;
+        }
+    }
+
+    public string Path { get; set; }
+    private readonly bool _isCustom;
+
+    public static Sound Custom(string path)
+    {
+        return new Sound(path, true);
+    }
+}

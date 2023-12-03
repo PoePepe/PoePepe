ba41e24e3c82dd144a08949dc980dc92062be148
Konstantin Spitsin <konstantin.spicin@simbirsoft.com>
2023-12-03 17:12:57 +0300

    draft for retry web socket connection.

--- Poe.LiveSearch/Models/OrderError.cs
+++ Poe.LiveSearch/Models/OrderError.cs
@@ -2,14 +2,22 @@
 
 public class OrderError
 {
-    public OrderError(long orderId, string errorMessage)
+    public OrderError(long orderId, string errorMessage, OrderErrorType errorType = OrderErrorType.Connection)
     {
         OrderId = orderId;
         ErrorMessage = errorMessage;
+        ErrorType = errorType;
     }
 
     public long OrderId { get; set; }
     public string ErrorMessage { get; set; }
+    public OrderErrorType ErrorType { get; set; }
+}
+
+public enum OrderErrorType
+{
+    Connection,
+    Process
 }
 
 public class OrderError<T> : OrderError
